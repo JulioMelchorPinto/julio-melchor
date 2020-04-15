6eb1a493f9f6faef85a72f5b933fa93095fe59ff
Julio Cesar Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-04-15 17:47:53 -0500

    Inicialize API data service

---
+++ src/app/shared/services/data.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private REST_API_SERVER = "http://localhost:3000";
+
+  constructor(private httpClient: HttpClient) { }
+}
