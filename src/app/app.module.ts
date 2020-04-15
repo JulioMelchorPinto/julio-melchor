388076622e5d45b7a7d71e6edf4b97404752946e
Julio Cesar Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-04-15 16:34:02 -0500

    Add about component and its routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import HttpClient Module
+// Import HttpClient Module
 import { HttpClientModule } from '@angular/common/http';
+// Routes components
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
