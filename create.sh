2c5ad6f78b2c934a9f82c450db6c8da7a63f8b4a
Julio Cesar Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-04-15 17:05:44 -0500

    Recover create.sh script

--- create.sh
+++ create.sh
@@ -4,7 +4,6 @@
 #
 #
 # © 2020 Julio César Melchor Pinto
-#
 # Create new project in Angular 9.1
 ng new julio-melchor
 # Open reciently created project directory
@@ -15,4 +14,3 @@ git remote add origin git@github.com:JulioMelchorPinto/julio-melchor.git
 git add -i
 git commit -m "Generate new Angular 9 project with Angular routing and CSS stylesheet format"
 git push -u origin master
-#
